4e7dd6e5c869a214c8b537a2fb418480ff375c2d
techtimefor <51377400+techtimefor@users.noreply.github.com>
2022-10-07 23:52:48 +0100

    note /copy can only copy 1 argument

--- sussycmds/help.ts
+++ sussycmds/help.ts
@@ -2,7 +2,7 @@ async function helpcmd(msg: any, sussy_secret: string, user: any) {
     const channel_id = "1578423740495040512"
     await msg.send(`Thank you ${user.username}! for trying me ;)`, channel_id, sussy_secret)
     await msg.send(`Commands [Alpha]`, channel_id, sussy_secret)
-    await msg.send(`/nft and /sus`, channel_id, sussy_secret)
+    await msg.send(`/nft and /sus and /copy`, channel_id, sussy_secret)
     await msg.send(`Huge thanks to Aelpxy for all the help making Sus Bot!`, channel_id, sussy_secret)
     await msg.send(`Debug Info: ID > ${user.id}, User Creation Date > ${user.createdAt}`, channel_id, sussy_secret)
 
