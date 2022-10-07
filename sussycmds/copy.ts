187b9b5a7a393cac1aa8c8ed05b8340eb8ae5dd5
techtimefor <51377400+techtimefor@users.noreply.github.com>
2022-10-07 23:28:10 +0100

    Copy command added usage /copy [args]

---
+++ sussycmds/copy.ts
@@ -0,0 +1,11 @@
+async function copycmd(msg: any, sussy_secret: string, user: any, args: any) {    
+    const channel_id = "1578423740495040512"
+    await msg.send(args, channel_id, sussy_secret)   
+
+
+};
+
+
+
+export default copycmd
+
